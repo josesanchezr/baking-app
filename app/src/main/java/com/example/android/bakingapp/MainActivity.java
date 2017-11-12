d38bdc5f08dfac28c6888c05de63268ebdce2a17
Jose Luis Sanchez <joseluis.sanchez84@gmail.com>
2017-11-12 16:27:11 -0500

    I saved the data used to the main activity

--- app/src/main/java/com/example/android/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.bakingapp;
 
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import com.example.android.bakingapp.adapter.RecipesAdapter;
 import com.example.android.bakingapp.api.RecipeDetailController;
 import com.example.android.bakingapp.data.Recipe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -23,6 +25,8 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String RECIPE_LIST = "recipe_list";
+
     @BindView(R.id.recipes_grid)
     GridView recipesGridView;
 
@@ -32,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
     CountingIdlingResource espressoTestIdlingResource = new CountingIdlingResource("Network_Call");
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        ArrayList<Parcelable> parcelablesRecipes = new ArrayList<>();
+        for (Recipe recipe : recipeList) {
+            parcelablesRecipes.add(recipe);
+        }
+        outState.putParcelableArrayList(RECIPE_LIST, parcelablesRecipes);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -39,22 +53,30 @@ public class MainActivity extends AppCompatActivity {
 
         espressoTestIdlingResource.increment();
 
-        recipeDetailController = new RecipeDetailController();
         if (savedInstanceState == null) {
-            recipeDetailController.recipeDetailApi.getRecipes().enqueue(recipesCallback);
-        }
 
-        recipesGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Recipe recipe = (Recipe) adapterView.getItemAtPosition(position);
-
-                Intent intent = new Intent(view.getContext(), RecipeDetailActivity.class);
-                intent.putExtra(RecipeDetailActivity.RECIPE_ACTIVITY, recipe);
-
-                startActivity(intent);
+            recipeDetailController = new RecipeDetailController();
+            if (savedInstanceState == null) {
+                recipeDetailController.recipeDetailApi.getRecipes().enqueue(recipesCallback);
             }
-        });
+
+            recipesGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                    Recipe recipe = (Recipe) adapterView.getItemAtPosition(position);
+
+                    Intent intent = new Intent(view.getContext(), RecipeDetailActivity.class);
+                    intent.putExtra(RecipeDetailActivity.RECIPE_ACTIVITY, recipe);
+
+                    startActivity(intent);
+                }
+            });
+        } else {
+            recipeList = savedInstanceState.getParcelableArrayList(RECIPE_LIST);
+            RecipesAdapter adapter = new RecipesAdapter(getApplicationContext(), recipeList);
+            recipesGridView.setAdapter(adapter);
+            recipesGridView.deferNotifyDataSetChanged();
+        }
     }
 
     private final Callback<List<Recipe>> recipesCallback = new Callback<List<Recipe>>() {
